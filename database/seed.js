2adee1986fbf7e45fdb89f00bfc6b843e7c9607b
Jude <jmm.maagad@gmail.com>
2024-12-19 09:47:34 +0800

    Added supabase insert data function

--- database/seed.js
+++ database/seed.js
@@ -5,8 +5,19 @@ import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
+// console.log(supabase)
 
-console.log(supabase)
+const seedProjects = async () => {
+  const name = faker.lorem.words(3)
+
+  await supabase.from('projects').insert({
+    name: name,
+    slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+    status: faker.helpers.arrayElement(['in-progress', 'completed']),
+    collaborators: faker.helpers.arrayElements([1, 2, 3]),
+  })
+}
+await seedProjects()
 
 // const personName = faker.person.fullName()
 // const personBio = faker.person.bio()
