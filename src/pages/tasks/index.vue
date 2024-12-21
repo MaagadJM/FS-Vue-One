53ed1387532c0bcdf8fb1d36829f15bd618752da
Jude <jmm.maagad@gmail.com>
2024-12-21 22:23:50 +0800

    Tasks: Fetching the data from Supabase and inserting it into shadcn data table

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -22,49 +22,67 @@ const tasks = ref<Tables<'tasks'>[] | null>(null)
     })()
 
 
-interface Payment {
-    id: string
-    amount: number
-    status: 'pending' | 'processing' | 'success' | 'failed'
-    email: string
-}
-
-const payments: Payment[] = [
-    {
-        id: '728ed52f',
-        amount: 100,
-        status: 'pending',
-        email: 'm@example.com',
-    },
-    {
-        id: '489e1d42',
-        amount: 125,
-        status: 'processing',
-        email: 'example@gmail.com',
-    },
-    // ...
-]
 
 
-const columns: ColumnDef<Payment>[] = [
+
+
+
+const columns: ColumnDef<Tables<'tasks'>>[] = [
     {
-        accessorKey: 'amount',
-        header: () => h('div', { class: 'text-right' }, 'Amount'),
+        accessorKey: 'name',
+        header: () => h('div', { class: 'text-left' }, 'Name'),
         cell: ({ row }) => {
-            const amount = Number.parseFloat(row.getValue('amount'))
-            const formatted = new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD',
-            }).format(amount)
 
-            return h('div', { class: 'text-right font-medium' }, formatted)
+
+            return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
         },
-    }
+    },
+    {
+        accessorKey: 'status',
+        header: () => h('div', { class: 'text-left' }, 'Status'),
+        cell: ({ row }) => {
+
+
+            return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+        },
+    },
+    {
+        accessorKey: 'due_date',
+        header: () => h('div', { class: 'text-left' }, 'Due Date'),
+        cell: ({ row }) => {
+
+
+            return h('div', { class: 'text-left font-medium' }, row.getValue('due_date'))
+        },
+    },
+    {
+        accessorKey: 'project_id',
+        header: () => h('div', { class: 'text-left' }, 'Project'),
+        cell: ({ row }) => {
+
+
+            return h('div', { class: 'text-left font-medium' }, row.getValue('project_id'))
+        },
+    },
+    {
+        accessorKey: 'collaborators',
+        header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+        cell: ({ row }) => {
+            return h(
+                'div', 
+                { class: 'text-left font-medium' }, 
+                // below this will convert it to an array of numbers, but still a string. so its not an array
+                JSON.stringify(row.getValue('collaborators'))
+            )
+        },
+    },
+    
 ]
 
 
 </script>
 
 <template>
-    <DataTable :columns="columns" :data="payments" />
+    <!-- ----- ( v-if ) only show this when the tasks exists ----- -->
+    <DataTable v-if="tasks" :columns="columns" :data="tasks" />
 </template>
