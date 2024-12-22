0b40fa222e6de81aeca177d006c209afc07422cd
Jude <jmm.maagad@gmail.com>
2024-12-22 14:18:30 +0800

    Projects: Fetching the data from Supabase and inserting it into shadcn data table

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,38 +1,58 @@
 <script setup lang="ts">
 
 import { supabase } from '@/lib/supabaseClient'
-import { ref } from 'vue'
+import { h, ref } from 'vue'
 import type { Tables } from '../../../database.types';
+import type { ColumnDef } from '@tanstack/vue-table';
+import DataTable from '@/components/ui/data-table/DataTable.vue';
 
-const projects = ref<Tables<'projects'>[] | null> (null)
+const projects = ref<Tables<'projects'>[] | null>(null)
 
-;(async () => {
-    const { data, error } = await supabase.from('projects').select()
+    ; (async () => {
+        const { data, error } = await supabase.from('projects').select()
 
-    if (error) console.log(error)
-
-
-    projects.value = data
-    console.log('projects: ', projects.value)
+        if (error) console.log(error)
 
 
-    return data
-})()
+        projects.value = data
+        console.log('projects: ', projects.value)
+
+
+        // return data
+    })()
+
+// the "[]" here <Tables<'projects'>>"[]" tell the typescript that it should be an array
+const columns: ColumnDef<Tables<'projects'>>[] = [
+    {
+        accessorKey: 'name',
+        header: () => h('div', { class: 'text-left' }, 'Name'),
+        cell: ({ row }) => {
+            return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
+        }
+    },
+    {
+        accessorKey: 'status',
+        header: () => h('div', { class: 'text-left' }, 'Status'),
+        cell: ({ row }) => {
+            return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+        }
+    },
+    {
+        accessorKey: 'collaborators',
+        header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+        cell: ({ row }) => {
+            return h('div', { class: 'text-left font-medium' },
+                // this JSON.stringify will convert it to an array of numbers, but still a string. so its not an array
+                JSON.stringify(row.getValue('collaborators'))
+            )
+        }
+    },
+
+]
 
 </script>
 
 <template>
-    <div>
-        <h1>Projects Page</h1>
-        <RouterLink to="/">Home</RouterLink>
-        <!-- if projects is not null and its array it will show array then if not do nothing -->
-        <!-- {{ projects ? projects[0] : '' }} -->
-
-        <ul>
-            <li v-for="project in projects" :key="project.id">
-                {{ project.name }}
-            </li>
-        </ul>
-
-    </div>
+    <!-- if projects exist then show its columns and data -->
+    <DataTable v-if="projects" :columns="columns" :data="projects" />
 </template>
