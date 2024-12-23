4d19499e52e1a60534f6d321289ef80ee1ac0e01
Jude <jmm.maagad@gmail.com>
2024-12-23 10:50:10 +0800

    Updated the router auto imports plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 
 import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 import autoprefixer from 'autoprefixer'
 import tailwind from 'tailwindcss'
@@ -26,7 +27,7 @@ export default defineConfig({
       imports: [
         // presets
         'vue',
-        'vue-router',
+        VueRouterAutoImports,
       ],
 
       // Defaults to './auto-imports.d.ts' when `typescript` is installed locally.
