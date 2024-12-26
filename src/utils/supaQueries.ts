ff388fc573edcea7b7398965d9220e820d3340c2
Jude <jmm.maagad@gmail.com>
2024-12-26 14:54:12 +0800

    feat: Added taskQuery & Create Dynamic Indvidual Task Page

--- src/utils/supaQueries.ts
+++ src/utils/supaQueries.ts
@@ -30,3 +30,19 @@ export const projectQuery = (slug: string) => supabase.from('projects').select(`
 .single()
 
 export type Project = QueryData<ReturnType<typeof projectQuery>>
+
+
+export const taskQuery = (id: string) => {
+    return supabase.from('tasks').select(`
+            *,
+            projects (
+                id,
+                name,
+                slug
+            )
+        `)
+        .eq('id', id)
+        .single()
+}
+
+export type Task = QueryData<ReturnType<typeof taskQuery>>
