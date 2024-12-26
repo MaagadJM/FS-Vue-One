fed8b8212e59f53e78aa891de538cf51e95636f1
Jude <jmm.maagad@gmail.com>
2024-12-26 12:56:02 +0800

    feat: Added description types. Made the Project Page collaborators & task dynamic

--- database/seed.js
+++ database/seed.js
@@ -4,7 +4,7 @@ import { fakerEN_US as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY);
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 // console.log(supabase)
 
 const logErrorAndExit = (tableName, error) => {
@@ -28,6 +28,7 @@ const seedProjects = async (numEntries) => {
     projects.push({
       name: name,
       slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+      description: faker.lorem.paragraphs(2),
       status: faker.helpers.arrayElement(['in-progress', 'completed']),
       collaborators: faker.helpers.arrayElements([1, 2, 3]),
     })
@@ -54,7 +55,7 @@ const seedTasks = async (numEntries, projectsIds) => {
       description: faker.lorem.paragraph(),
       due_date: faker.date.future(),
       project_id: faker.helpers.arrayElement(projectsIds),
-      collaborators: faker.helpers.arrayElements([1, 2, 3])
+      collaborators: faker.helpers.arrayElements([1, 2, 3]),
     })
   }
 
@@ -66,8 +67,6 @@ const seedTasks = async (numEntries, projectsIds) => {
 
   return data
 }
-
-
 
 const seedDatabase = async (numEntriesPerTable) => {
   const projectsIds = (await seedProjects(numEntriesPerTable)).map((project) => project.id)
